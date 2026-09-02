6f146e37b68546c2fc13705eb9d9356b33a91bf0
Kyle Parker <kylerparker1500@gmail.com>
2026-09-02 00:38:54 -0700

    Add shop item for recycler

---
+++ app/src/main/java/com/example/cst438_project1_team5/ui/shop/ShopItem.kt
@@ -0,0 +1,22 @@
+package com.example.cst438_project1_team5.ui.shop
+
+import androidx.annotation.DrawableRes
+import com.example.cst438_project1_team5.R
+
+data class ShopItem(
+    val id: Int,
+    val title: String,
+    val description: String,
+    @DrawableRes val imageResId: Int
+)
+
+val placeholderShopItems: List<ShopItem> = List(8) { index ->
+    val itemNumber = index + 1
+
+    ShopItem(
+        id = itemNumber,
+        title = "Item $itemNumber",
+        description = "Description for item $itemNumber",
+        imageResId = R.drawable.shop_item_placeholder
+    )
+}
