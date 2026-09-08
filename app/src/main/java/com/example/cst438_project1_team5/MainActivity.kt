45447282e03602b67d27655a7ae6275d66801e98
Joseph <jtrbg123@gmail.com>
2026-09-07 17:47:45 -0700

    This feature was implemented while working on Issue #4.

--- app/src/main/java/com/example/cst438_project1_team5/MainActivity.kt
+++ app/src/main/java/com/example/cst438_project1_team5/MainActivity.kt
@@ -55,7 +55,6 @@ enum class AuthMode {
     SignUp
 }
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
