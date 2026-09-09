7771cef3318638737a393ceebf5a665ea4cc01d6
Eashwar Sridharan <hypersound16@gmail.com>
2026-09-08 19:23:57 -0700

    Unit test in malAPItest somehow was lost, I put it back for now using local history

--- app/src/androidTest/java/com/example/cst438_project1_team5/malAPItest.kt
+++ app/src/androidTest/java/com/example/cst438_project1_team5/malAPItest.kt
@@ -1,1 +1,20 @@
+package com.example.cst438_project1_team5
 
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class malAPITest {
+    @Test
+    fun getList() {
+        val repository = malAPIRepository()
+        val result = repository.getList("Marxeru")
+        println(result)
+        assertTrue(result.isSuccess)
+    }
+}
