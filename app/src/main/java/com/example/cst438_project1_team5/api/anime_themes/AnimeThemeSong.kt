32d2244f3fd3b067c159c9bf97af2b88e5ea5898
Kyle Parker <kylerparker1500@gmail.com>
2026-09-14 23:36:40 -0700

    Added data class to anime themes api to get video

--- app/src/main/java/com/example/cst438_project1_team5/api/anime_themes/AnimeThemeSong.kt
+++ app/src/main/java/com/example/cst438_project1_team5/api/anime_themes/AnimeThemeSong.kt
@@ -1,6 +1,7 @@
 package com.example.cst438_project1_team5.api.anime_themes
 
 import com.google.gson.annotations.SerializedName
+import java.util.Date
 
 data class AnimeThemeSong(
     val id: Int,
@@ -28,4 +29,40 @@ data class AnimeThemeSongsResponse(
 data class AnimeThemeSongResponse(
     @SerializedName("audio")
     val song: AnimeThemeSong
+)
+
+// for video data
+// video format
+enum class Source {
+    WEB, RAW, BD, DVD, VHS, LD
+}
+
+// The degree to which the sequence and episode
+// content overlap.
+enum class Overlap {
+    None, Transition, Over
+}
+
+data class AnimeVideo(
+    val id: Int,
+    val basename: String,
+    val filename: String,
+    val path: String,
+    val size: Int,
+    val mimetype: String,
+    val resolution: Int?,
+    @SerializedName("no_credit")
+    val nc: Boolean,
+    val subbed: Boolean,
+    val lyrics: Boolean,
+    @SerializedName("uncensored")
+    val uncen: Boolean, // necessary to have this be false (default is true)
+    val source: Source,
+    val overlap: Overlap,
+    val tags: String,
+    val link: String,
+    val view_count: Int,
+    val created_at: Date,
+    val updated_at: Date,
+    val deleted_at: Date,
 )
